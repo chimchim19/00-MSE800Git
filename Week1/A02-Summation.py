1755a377e08a3bde1394c1f87d901081d8c3240d
chimchim19 <270765080@yoobeestudent.ac.nz>
2025-08-04 18:41:46 +1200

    Update A02-Summation.py with code comments

--- Week1/A02-Summation.py
+++ Week1/A02-Summation.py
@@ -2,16 +2,19 @@ def main():
   user_input = input("Input a number between 1 and 50: ")
   intNum = int(user_input)
 
-  # check if less than 0, raise a message
+  # check if less than or equal to 0, raise a message
   if intNum <= 0:
     return "Invalid input. Give a number betwwen 1 and 50."
   else:
+    # set initial values
     result = 0
     counter = 1
+    
+    # loop while less than or equal to input number
     while counter <= intNum:
       if counter % 2 == 0:
         print(counter)
-        result = result + counter
+        result = result + counter # add if even number
       counter = counter + 1
     
     print("The sum of positive even numbers from 1 to", intNum, "is:", result)
