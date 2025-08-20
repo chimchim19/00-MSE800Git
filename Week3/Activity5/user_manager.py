6b26c008b811a7f14b6bd920c7eb820cdee81557
chimchim19 <270765080@yoobeestudent.ac.nz>
2025-08-20 15:50:49 +1200

    Create user_manager.py

---
+++ Week3/Activity5/user_manager.py
@@ -0,0 +1,43 @@
+"""
+Week 3 - Activity 5: update the sample code "Sample_code_SQLite3"
+Please add a new table named "Students" with three columns: Stu_ID, Stu_name, and Stu_address. 
+Insert two sample records into Students, then display all rows from both the Users and Students tables.
+"""
+
+from database import create_connection
+import sqlite3
+
+def add_user(name, email):
+    conn = create_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", (name, email))
+        conn.commit()
+        print(" User added successfully.")
+    except sqlite3.IntegrityError:
+        print(" Email must be unique.")
+    conn.close()
+
+def view_users():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def search_user(name):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE name LIKE ?", ('%' + name + '%',))
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def delete_user(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+    conn.commit()
+    conn.close()
+    print("🗑️ User deleted.")
