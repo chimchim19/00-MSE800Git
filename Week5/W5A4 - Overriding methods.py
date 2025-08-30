9b9d622e81ba5003c8030483ba80039661cde022
chimchim19 <270765080@yoobeestudent.ac.nz>
2025-08-31 08:21:47 +1200

    Create W5A4 - Overriding methods.py

---
+++ Week5/W5A4 - Overriding methods.py
@@ -0,0 +1,45 @@
+class Person:
+    def __init__(self, name, address, age, id):
+        self.name = name
+        self.address = address
+        self.age = age
+        self.id = id
+
+    def showDetails(self):
+        print("\nSuperclass display:")
+        print(f"{self.name}, age {self.age}, lives at {self.address}")
+
+# Staff (subclass) inherits from Person (superclass)
+class Staff(Person):
+    def __init__(self, name, address, age, id, code, rate):
+        
+        # calls super() to initialize inherited attributes from parent class
+        super().__init__(name, address, age, id)
+
+        # initialize child class' own attributes
+        self.taxcode = code
+        self.payrate = rate
+
+    def showDetails(self):
+        print("\nSubclass display:")
+        print(f"{self.name}, age {self.age}, lives at {self.address}")
+        print(f"{self.name} has tax code {self.taxcode} at {self.payrate} hourly pay rate")
+
+if __name__ == "__main__":
+
+    person1 = Person("Tom", "123 Apple Street", 35, 1005)
+    staff1 = Staff("Ana", "55 Sunrise Blvd", 25, 1008, "M", 45)
+
+    # object typed reference to superclass
+    #print(f"{person1.name}, age {person1.age}, lives at {person1.address}")
+    
+    # this is showDetails() of the superclass
+    person1.showDetails()
+
+    # object typed reference to subclass
+    #print(f"{staff1.name}, age {staff1.age}, lives at {staff1.address}")
+    #print(f"{staff1.name} has tax code {staff1.taxcode} at {staff1.payrate} hourly pay rate")
+
+    # this is showDetails() of the subclass
+    staff1.showDetails()
+    
