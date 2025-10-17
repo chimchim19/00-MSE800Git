e3a1c3557b61547b4bcdbb0b3eef2703bac55371
chimchim19 <270765080@yoobeestudent.ac.nz>
2025-10-18 11:45:41 +1300

    Create w12_activity1-2.py

---
+++ Week12/w12_activity1-2.py
@@ -0,0 +1,20 @@
+""" Week 12 - Activity 1.1: Flask - Variable path """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    return "<p>Hello, Flask!</p>"
+
+@app.route("/bye")
+def bye():
+    return "<p>Bye, Flask!</p>"
+
+@app.route("/username/<name>")
+def learn(name):
+    return f"{name} is learning Flask!"
+
+if __name__ == '__main__':
+    app.run(debug = True)
