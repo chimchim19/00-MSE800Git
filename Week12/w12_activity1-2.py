7739b9752300cb034a3de1375b8eb749657a48a3
chimchim19 <270765080@yoobeestudent.ac.nz>
2025-10-18 11:55:23 +1300

    Update w12_activity1-2.py

--- Week12/w12_activity1-2.py
+++ Week12/w12_activity1-2.py
@@ -1,4 +1,5 @@
 """ Week 12 - Activity 1.1: Flask - Variable path """
+""" Week 12 - Activity 1.2: Dynamic routing """
 
 from flask import Flask
 
@@ -15,6 +16,12 @@ def bye():
 @app.route("/username/<name>")
 def learn(name):
     return f"{name} is learning Flask!"
+
+""" This accepts a number and displays the value when squared """
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"The square of {number} is {number**2}"
+
 
 if __name__ == '__main__':
     app.run(debug = True)
